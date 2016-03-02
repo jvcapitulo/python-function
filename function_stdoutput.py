74d42aaf763806d260454ef5c843e10194c77e18
jvcapitulo <capitulo88@gmail.com>
2016-03-01 21:01:48 -0500

    changed function name from MyFunc to Funcout

---
+++ function_stdoutput.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+def MyFunc (no1,no2):
+        
+        print "Hi! My name is %s and my age is %s"%(no1,no2)
+        
+        print "Hi! My name is",no1+" and my age is",no2
+        
+        print "Hi! My name is {} and my age is {}".format(no1,no2)
+        
+MyFunc("John Vincent Capitulo",19)    
+
+
+
+
+
+
+
